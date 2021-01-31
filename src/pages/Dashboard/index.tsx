415dd9cd4f2cc21915e8051fbd781dddeb2371c5
danubiobwm <danubio.bwm@gmail.com>
2021-01-30 21:23:27 -0300

    feat: :construction_worker: teste Git
    TestChama
    
    testeChama

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,90 @@
+import React, { useState, useEffect, FormEvent } from 'react';
+import { Link } from 'react-router-dom';
+import { FiChevronRight } from 'react-icons/fi';
+import logoImg from '../../assets/logo.svg';
+import { Title, Form, Repositories, Error } from './styles';
+import api from '../../services/api';
+
+interface Repositories {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
+const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [inputError, setInputError] = useState('');
+  const [repositories, setRepositories] = useState<Repositories[]>(() => {
+    const storagedRepositories = localStorage.getItem('@Gitguh:repositories');
+
+    if (storagedRepositories) {
+      return JSON.parse(storagedRepositories);
+    }
+    return [];
+  });
+
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault();
+    if (!newRepo) {
+      setInputError('Digite o autor/nome do repositório');
+      return;
+    }
+
+    try {
+      const response = await api.get(`repos/${newRepo}`);
+      const repository = response.data;
+      setRepositories([...repositories, repository]);
+      setNewRepo('');
+    } catch (error) {
+      setInputError('Erro na busca por esse repositório');
+    }
+  }
+
+  useEffect(() => {
+    localStorage.setItem('@Gitguh:repositories', JSON.stringify(repositories));
+  }, [repositories]);
+
+  return (
+    <>
+      <img src={logoImg} alt="Github Explore" />
+      <Title>Explore Users no Github.</Title>
+
+      <Form hasError={!!inputError} onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={e => setNewRepo(e.target.value)}
+          placeholder="Digite Aqui..."
+        />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      {inputError && <Error>{inputError}</Error>}
+
+      <Repositories>
+        {repositories.map(repository => (
+          <Link
+            key={repository.full_name}
+            to={`/repository/${repository.full_name}`}
+          >
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+            <FiChevronRight size={20} />
+          </Link>
+        ))}
+      </Repositories>
+    </>
+  );
+};
+
+export default Dashboard;
---
+++ src/styles/global.ts
@@ -0,0 +1,30 @@
+import { createGlobalStyle } from 'styled-components';
+import gitBackground from '../assets/gitBackground.svg';
+
+export default createGlobalStyle`
+ *{
+   margin:0;
+   padding:0;
+   box-sizing: border-box;
+   outline:0;
+ }
+
+ body{
+  background: #F0F0F5 url(${gitBackground}) no-repeat 70% top;
+  -webkit-font-smoothinf: antialiased;
+
+ }
+ body, input, button{
+  font-family: 'Roboto', sans-serif;
+ }
+
+ #root {
+    max-width: 960px;
+    margin: 0 auto;
+    padding: 40px 20px;
+  }
+
+ butoon{
+   cursor: pointer;
+ }
+`;
